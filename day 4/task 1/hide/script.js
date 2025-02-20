4c08143f827825032182373998021180a7a7ce0e
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-02-20 22:42:16 +0530

    Add files via upload

---
+++ day 4/task 1/hide/script.js
@@ -0,0 +1,13 @@
+function Content(id) {
+    var content = document.getElementById(id);
+    if (content) {
+        content.style.display = "block"; // Show the element
+    }
+}
+
+function Content(id) {
+    var content = document.getElementById(id);
+    if (content) {
+        content.style.display = "none"; // Hide the element
+    }
+}
