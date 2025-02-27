22da164046428ce8f56fb06f263da4fbcfd216f7
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-02-27 12:32:40 +0530

    Update studentcard.jsx

--- day 8/task 4/studentcard.jsx
+++ day 8/task 4/studentcard.jsx
@@ -1,9 +1,9 @@
 export default function HelloWorld() {
 import "./StudentCard.css";
 import StudentCard from "./StudentCard";
-export default function StudentCard({ name, major, year }) {
+export StudentContainer({ name, major, year }) => {
   return (
-    <div className="student-card">
+<div className="student-list">
       <h2 className="student-name">name:Abidhashree{name}</h2>
       <p className="student-major">Major:IT {major}</p>
       <p className="student-year">Year:3rd {year}</p>
