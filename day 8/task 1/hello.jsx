707604c13eab960eb5677a00ddcf9e38219dc726
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-02-27 12:02:26 +0530

    Create hello.jsx

---
+++ day 8/task 1/hello.jsx
@@ -0,0 +1,9 @@
+export default function HelloWorld() {
+import "./StudentCard.css";
+import StudentCard from "./StudentCard";
+
+export default function StudentList({ students }) {
+  return (
+    <div >
+      Hello, Abidhashree
+</div>
