8a01f25232ca2902e8e7413459e88aa954cbe4cc
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-02-27 12:07:39 +0530

    Create studentname.jsx

---
+++ day 8/task 2/studentname.jsx
@@ -0,0 +1,10 @@
+export default function HelloWorld() {
+import "./StudentCard.css";
+import StudentCard from "./StudentCard";
+export default function StudentName({ name }) {
+  return (
+    <div className="student-name-container">
+      <h2>Student Name:Abidhashree</h2>
+    </div>
+  );
+}
